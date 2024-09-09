ac58f0faf19306f34def6b92f7fdad7f954040fc
kovacsnandor <kovacsnandor@gmail.com>
2024-09-09 10:55:23 +0200

    Bootstrap, icons telepítés, teszt

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,25 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { RouterLink, RouterView } from "vue-router";
+import HelloWorld from "./components/HelloWorld.vue";
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <img
+      alt="Vue logo"
+      class="logo"
+      src="@/assets/logo.svg"
+      width="125"
+      height="125"
+    />
 
     <div class="wrapper">
+      <!-- Bootstrap teszt -->
+      <div class="alert alert-primary" role="alert">
+        <i class="bi bi-yin-yang"></i>
+        A simple primary alert—check it out!
+      </div>
+
       <HelloWorld msg="You did it!" />
 
       <nav>
--- src/main.js
+++ src/main.js
@@ -6,6 +6,12 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+//Bootstrap: css, js
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap"
+//Icons: css
+import "bootstrap-icons/font/bootstrap-icons.min.css"
+
 const app = createApp(App)
 
 app.use(createPinia())
