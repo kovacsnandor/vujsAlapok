1898b15f2f4e0f6c5fcf2df8f6d937726b99a598
kovacsnandor <kovacsnandor@gmail.com>
2024-09-13 08:56:33 +0200

    Main tisztítás

--- src/App.vue
+++ src/App.vue
@@ -4,17 +4,10 @@ import { RouterLink, RouterView } from "vue-router";
 
 <template>
   <div class="container-fluid my-border">
-    <h1>Direktívák</h1>
+    <h1>Témakör</h1>
     <nav class="my-border p-4">
       <RouterLink to="/">Home</RouterLink> | 
-      <RouterLink to="/databind">Data Bind</RouterLink> | 
-      <RouterLink to="/vif">v-if</RouterLink> | 
-      <RouterLink to="/vshow">v-show</RouterLink> | 
-      <RouterLink to="/vfor">v-for</RouterLink> | 
-      <RouterLink to="/von">v-on</RouterLink> | 
-      <RouterLink to="/cssbinding">css binding</RouterLink> | 
-      <RouterLink to="/computed">Computed</RouterLink> | 
-      <RouterLink to="/watch">Watch</RouterLink> | 
+      <RouterLink to="/about">About</RouterLink> | 
     </nav>
     <div class="my-border p-2" >
       <RouterView />
--- src/router/index.js
+++ src/router/index.js
@@ -10,45 +10,10 @@ const router = createRouter({
       component: HomeView
     },
     {
-      path: '/databind',
-      name: 'databind',
-      component: () => import('../views/DataBind.vue')
+      path: '/about',
+      component: () => import('../views/About.vue')
     },
-    {
-      path: '/vif',
-      name: 'vif',
-      component: () => import('../views/Vif.vue')
-    },
-    {
-      path: '/vshow',
-      name: 'vshow',
-      component: () => import('../views/Vshow.vue')
-    },
-    {
-      path: '/vfor',
-      name: 'vfor',
-      component: () => import('../views/Vfor.vue')
-    },
-    {
-      path: '/von',
-      name: 'von',
-      component: () => import('../views/Von.vue')
-    },
-    {
-      path: '/cssbinding',
-      name: 'cssbinding',
-      component: () => import('../views/Cssbinding.vue')
-    },
-    {
-      path: '/computed',
-      name: 'computed',
-      component: () => import('../views/Computed.vue')
-    },
-    {
-      path: '/watch',
-      name: 'watch',
-      component: () => import('../views/Watch.vue')
-    }
+    
   ]
 })
 
